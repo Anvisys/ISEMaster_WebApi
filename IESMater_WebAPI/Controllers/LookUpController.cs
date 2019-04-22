2519e8911a75963ef5e9c71a8e1daa45675aa702
AmitBansal73 <43637892+AmitBansal73@users.noreply.github.com>
2019-04-22 17:36:40 +0530

    Master Initiated

---
+++ IESMater_WebAPI/Controllers/LookUpController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Data.Entity;
+
+namespace IESMater_WebAPI.Controllers
+{
+   
+
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+
+    [RoutePrefix("api/Lookup")]
+    public class LookUpController : ApiController
+    {
+
+        
+
+        [Route("University/{UnivName}")]
+        [HttpGet]
+        public IEnumerable<IESUniversity> GetUniversity(String UnivName)
+        {
+            var context = new xPenEntities();
+            var soc = (from s in context.IESUniversities
+                       where s.Name.Contains(UnivName)
+                       select s).ToList();
+
+            return soc;
+        }
+
+        [Route("College/{CollegeName}/{UniversityID}")]
+        [HttpGet]
+        public IEnumerable<IESCollege> Get(int UniversityID, String CollegeName)
+        {
+            var context = new xPenEntities();
+            var soc = (from s in context.IESColleges
+                       where s.UnivID == UniversityID && s.College_Name.Contains(CollegeName)
+                       select s).ToList();
+
+            return soc;
+        }
+
+        [Route("Stream/{UniversityID}/{StreamName}")]
+        [HttpGet]
+        public IEnumerable<IESStream> GetStream(int UniversityID, String StreamName)
+        {
+            var context = new xPenEntities();
+            var soc = (from s in context.IESStreams
+                       where s.UnivID == UniversityID && s.Stream_Name.Contains(StreamName)
+                       select s).ToList();
+
+            return soc;
+        }
+
+        [Route("Stream/{UniversityID}")]
+        [HttpGet]
+        public IEnumerable<IESStream> GetAllStream(int UniversityID, String StreamName)
+        {
+            var context = new xPenEntities();
+            var soc = (from s in context.IESStreams
+                       where s.UnivID == UniversityID && s.Stream_Name.Contains(StreamName)
+                       select s).ToList();
+
+            return soc;
+        }
+
+     
+    }
+}
---
+++ IESMater_WebAPI/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using System.Web.Http.Cors;
+
+
+namespace IESMater_WebAPI.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+
+    [RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {
+        [Route("{Email}")]
+        [HttpGet]
+        public IESUserProfile GetUserProfile(String Email)
+        {
+            var context = new xPenEntities();
+            var soc = (from s in context.IESUserProfiles
+                       where s.Email == Email
+                       select s).FirstOrDefault();
+
+            return soc;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [Route("Register")]
+        [HttpGet]
+        public IHttpActionResult Post([FromBody]IESUserProfile profile)
+        {
+            try
+            {
+                var context = new xPenEntities();
+                context.IESUserProfiles.Add(profile);
+                context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception Ex)
+            {
+                return InternalServerError();
+            }
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
