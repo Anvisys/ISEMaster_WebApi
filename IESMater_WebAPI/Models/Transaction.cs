da205553b02dfc32e85e9bf84d00dc6de625e2b0
AmitBansal73 <43637892+AmitBansal73@users.noreply.github.com>
2019-05-22 16:01:10 +0530

    Create Transaction.cs

---
+++ IESMater_WebAPI/Models/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IESMater_WebAPI.Models
+{
+    public class Transaction
+    {
+        public int UserID;
+        public int OrderID;
+        public int Paid;
+        public string CheckSum;
+
+    }
+}
